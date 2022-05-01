91036f4ed575576fc9ac87b216fbf0610d73f619
Iev Khov <ievkhov1@gmail.com>
2022-04-30 18:44:52 -0700

    Generate migration file

---
+++ address/urls.py
@@ -0,0 +1,11 @@
+from posixpath import basename
+from django.urls import include, path
+from rest_framework import routers
+from address.views import DistanceViewSet
+
+router = routers.SimpleRouter()
+router.register(r"distance", DistanceViewSet, basename="distance")
+
+urlpatterns = [
+  path("", include(router.urls)),
+]
